dbeccda30ad84e9b2a9abd62de219c005be19569
Vivek Singh <viveksingh9301@gmail.com>
2025-12-18 12:36:21 +0530

    Add restaurant data fetching in RestaurantList component
    - Implemented a function to fetch restaurant data from a JSON file.
    - Utilized useEffect to call the fetch function on component mount, logging the retrieved restaurant list.

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,6 +1,16 @@
-import type React from "react";
+import { useEffect } from "react";
 
 const RestaurantList: React.FC = () => {
+  async function getRestaurantList() {
+    const res = await fetch("/data/restaurant-list.json");
+    const restaurantList = await res.json();
+    console.log(restaurantList);
+  }
+
+  useEffect(() => {
+    getRestaurantList();
+  }, []);
+
   return (
     <section className="min-h-screen">
       <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">Restaurants near you</h2>
