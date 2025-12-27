b7f66ffc4568fbb490243ad29e116c0d6e15dea9
Vivek Singh <viveksingh9301@gmail.com>
2025-12-27 10:39:02 +0530

    Implement restaurant grid layout in RestaurantList component
    - Added a grid layout to display restaurant cards dynamically.
    - Integrated RestaurantCard component to showcase individual restaurant details.

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type IRestaurant from "../models/restaurant";
+import RestaurantCard from "./RestaurantCard";
 
 const RestaurantList: React.FC = () => {
   const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
@@ -22,7 +23,20 @@ const RestaurantList: React.FC = () => {
   return (
     <section className="min-h-screen">
       <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">Restaurants near you</h2>
-      <h3>Restaurant list will appear here.</h3>
+      {/* Grid */}
+      <div
+        className="
+            grid gap-6
+            grid-cols-1
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+          "
+      >
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} restaurant={restaurant} />
+        ))}
+      </div>
     </section>
   );
 };
