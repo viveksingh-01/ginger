d60aa40d47a34c66420093d831dff53bfa93c961
Vivek Singh <viveksingh9301@gmail.com>
2025-12-28 12:27:58 +0530

    Update loading state in RestaurantList component with skeleton placeholders
    - Replaced the loading text with a grid of RestaurantListSkeleton components to visually represent the loading state while data is being fetched.

--- src/components/RestaurantList.tsx
+++ src/components/RestaurantList.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import type IRestaurant from "../models/restaurant";
 import RestaurantCard from "./RestaurantCard";
+import RestaurantListSkeleton from "./RestaurantListSkeleton";
 
 const RestaurantList: React.FC = () => {
   const [restaurants, setRestaurants] = useState<IRestaurant[]>([]);
@@ -27,7 +28,19 @@ const RestaurantList: React.FC = () => {
       <section className="min-h-screen">
         <div className="container mx-auto px-6 py-10">
           <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">Restaurants near you</h2>
-          <div>Loading...</div>
+          <div
+            className="
+              grid gap-6
+              grid-cols-1
+              sm:grid-cols-2
+              md:grid-cols-3
+              lg:grid-cols-4
+            "
+          >
+            {Array.from({ length: 8 }).map((_, index) => (
+              <RestaurantListSkeleton key={index} />
+            ))}
+          </div>
         </div>
       </section>
     );
