de5c3f300f1c553af40a7cbd796329e29a379662
Vivek Singh <viveksingh9301@gmail.com>
2026-02-07 21:06:28 +0530

    Update routing in main.tsx to use restaurantId parameter for RestaurantMenu

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App.tsx'
-import RestaurantList from './components/RestaurantList.tsx'
-import RestaurantMenu from './components/RestaurantMenu.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.tsx';
+import RestaurantList from './components/RestaurantList.tsx';
+import RestaurantMenu from './components/RestaurantMenu.tsx';
+import './index.css';
 
 const router = createBrowserRouter([
   {
@@ -12,13 +12,13 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: '/', element: <RestaurantList /> },
-      { path: 'restaurant/:id', element: <RestaurantMenu /> }
-    ]
+      { path: 'restaurant/:restaurantId', element: <RestaurantMenu /> },
+    ],
   },
-])
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
