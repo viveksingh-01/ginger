0e08c29036266701f8a4271a07f2e3ad502f735c
Vivek Singh <viveksingh9301@gmail.com>
2026-02-09 01:28:04 +0530

    Refactor RestaurantMenu component to destructure menu item properties for cleaner code and improved readability

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -13,22 +13,14 @@ const RestaurantMenu: React.FC = () => {
       <div className="container mx-auto px-6 py-10 max-w-3xl">
         <h2 className="mb-12 text-3xl font-bold text-gray-900 dark:text-white">Test Restaurant</h2>
         <ul className="space-y-6">
-          {menu?.items.map(item => (
+          {menu?.items.map(({ id, name, price, imageId }) => (
             <>
-              <li key={item.id} className="flex justify-between items-start">
+              <li key={id} className="flex justify-between items-start">
                 <div className="pr-4">
-                  <h3 className="font-semibold text-lg text-gray-900 dark:text-white">
-                    {item.name}
-                  </h3>
-                  <p className="text-gray-900 dark:text-gray-400 mt-1">₹{item.price / 100}</p>
+                  <h3 className="font-semibold text-lg text-gray-900 dark:text-white">{name}</h3>
+                  <p className="text-gray-900 dark:text-gray-400 mt-1">₹{price / 100}</p>
                 </div>
-                {item.imageId && (
-                  <img
-                    src={IMAGE_URL + item.imageId}
-                    alt={item.name}
-                    className="w-40 h-36 object-cover rounded-xl"
-                  />
-                )}
+                {imageId && <img src={IMAGE_URL + imageId} alt={name} className="w-40 h-36 object-cover rounded-xl" />}
               </li>
               <hr className="text-gray-300" />
             </>
