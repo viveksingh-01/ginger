361a59c7811e3953099cd15352f34e7d9e2637f9
Vivek Singh <viveksingh9301@gmail.com>
2026-02-10 01:56:47 +0530

    Add ratings display to menu items in RestaurantMenu component

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -1,3 +1,4 @@
+import { FaStar } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
 import { IMAGE_URL } from '../constants';
 import useRestaurantMenu from '../hooks/useRestaurantMenu';
@@ -13,7 +14,7 @@ const RestaurantMenu: React.FC = () => {
       <div className="container mx-auto px-6 py-10 max-w-3xl">
         <h2 className="mb-12 text-3xl font-bold text-gray-900 dark:text-white">Test Restaurant</h2>
         <ul className="space-y-6">
-          {menu?.items.map(({ id, name, price, finalPrice, itemPriceStrikeOff, imageId }) => (
+          {menu?.items.map(({ id, name, price, finalPrice, itemPriceStrikeOff, imageId, ratings }) => (
             <>
               <li key={id} className="flex justify-between items-start">
                 <div className="pr-4">
@@ -22,6 +23,10 @@ const RestaurantMenu: React.FC = () => {
                     {itemPriceStrikeOff && <span className="text-gray-400 line-through">₹{price / 100}</span>} ₹
                     {finalPrice / 100}
                   </p>
+                  <div className="mt-2 flex items-center gap-0.5 text-sm">
+                    <FaStar className="text-green-700" />
+                    <span className="font-bold text-green-700">{ratings.rating}</span>({ratings.ratingCountV2})
+                  </div>
                 </div>
                 {imageId && <img src={IMAGE_URL + imageId} alt={name} className="w-40 h-36 object-cover rounded-xl" />}
               </li>
