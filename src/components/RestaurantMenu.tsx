49b7d896375eee076293934fcfb12f782416b6cd
Vivek Singh <viveksingh9301@gmail.com>
2026-03-02 13:53:30 +0530

    Refactor RestaurantMenu to utilize MenuItem component for rendering menu items
    - Updated the RestaurantMenu component to replace inline rendering of menu items with the new MenuItem component, improving code readability and maintainability.

--- src/components/RestaurantMenu.tsx
+++ src/components/RestaurantMenu.tsx
@@ -1,7 +1,7 @@
 import { FaCircle, FaStar } from 'react-icons/fa';
 import { useParams } from 'react-router-dom';
-import { IMAGE_URL } from '../constants';
 import useRestaurantDetails from '../hooks/useRestaurantDetails';
+import MenuItem from './MenuItem';
 
 const RestaurantMenu: React.FC = () => {
   const { restaurantId } = useParams();
@@ -30,35 +30,9 @@ const RestaurantMenu: React.FC = () => {
           </div>
         </div>
         <ul className="space-y-6">
-          {restaurant?.menu?.map(
-            ({ id, name, description, price, finalPrice, itemPriceStrikeOff, imageId, ratings }) => (
-              <>
-                <li key={id} className="flex justify-between items-start">
-                  <div className="pr-4">
-                    <h3 className="font-semibold text-lg text-gray-900 dark:text-white">{name}</h3>
-                    <p className="dark:text-gray-400 mt-1">
-                      {itemPriceStrikeOff && <span className="text-gray-400 line-through">₹{price / 100}</span>} ₹
-                      {finalPrice / 100}
-                    </p>
-                    <div className="mt-2 flex items-center gap-0.5 text-sm">
-                      <FaStar className="text-green-700" />
-                      <span className="font-bold text-green-700">{ratings.rating}</span>({ratings.ratingCountV2})
-                    </div>
-                    <p className="mt-2 text-gray-600 text-sm">{description}</p>
-                  </div>
-                  <div className="relative w-40 h-36 shrink-0">
-                    {imageId && (
-                      <img src={IMAGE_URL + imageId} alt={name} className="w-full h-full object-cover rounded-xl" />
-                    )}
-                    <button className="absolute -bottom-3 left-1/2 -translate-x-1/2 bg-white text-green-600 font-bold px-8 py-1 rounded-lg shadow-md border border-gray-200">
-                      ADD
-                    </button>
-                  </div>
-                </li>
-                <hr className="text-gray-300" />
-              </>
-            )
-          )}
+          {restaurant?.menu?.map(item => (
+            <MenuItem item={item} key={item.id} />
+          ))}
         </ul>
       </div>
     </section>
