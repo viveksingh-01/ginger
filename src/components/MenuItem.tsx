e305dc9a0e2e04c7519caada06391da2da388bef
Vivek Singh <viveksingh9301@gmail.com>
2026-03-02 13:51:26 +0530

    Add MenuItem component for displaying menu items
    - Introduced a new MenuItem component that renders individual menu items with details such as name, description, price, and ratings.
    - Included an image and an "ADD" button for adding items to the cart, enhancing the user interface for the menu page.

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,38 @@
+import { FaStar } from 'react-icons/fa';
+import { IMAGE_URL } from '../constants';
+import type { IMenuItem } from '../models/menu';
+
+type MenuItemProps = {
+  item: IMenuItem;
+};
+
+const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
+  const { id, name, description, price, finalPrice, itemPriceStrikeOff, imageId, ratings } = item;
+  return (
+    <>
+      <li key={id} className="flex justify-between items-start">
+        <div className="pr-4">
+          <h3 className="font-semibold text-lg text-gray-900 dark:text-white">{name}</h3>
+          <p className="dark:text-gray-400 mt-1">
+            {itemPriceStrikeOff && <span className="text-gray-400 line-through">₹{price / 100}</span>} ₹
+            {finalPrice / 100}
+          </p>
+          <div className="mt-2 flex items-center gap-0.5 text-sm">
+            <FaStar className="text-green-700" />
+            <span className="font-bold text-green-700">{ratings.rating}</span>({ratings.ratingCountV2})
+          </div>
+          <p className="mt-2 text-gray-600 text-sm">{description}</p>
+        </div>
+        <div className="relative w-40 h-36 shrink-0">
+          {imageId && <img src={IMAGE_URL + imageId} alt={name} className="w-full h-full object-cover rounded-xl" />}
+          <button className="absolute -bottom-3 left-1/2 -translate-x-1/2 bg-white text-green-600 font-bold px-8 py-1 rounded-lg shadow-md border border-gray-200">
+            ADD
+          </button>
+        </div>
+      </li>
+      <hr className="text-gray-300" />
+    </>
+  );
+};
+
+export default MenuItem;
