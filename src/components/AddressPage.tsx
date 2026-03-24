2008b81970bda7c1fb9830e18e49c3f14e2b2ed5
Vivek Singh <viveksingh9301@gmail.com>
2026-03-24 14:49:16 +0530

    Enhance AddressPage component with stored address selection
    - Added useEffect to automatically select and display the stored address from Redux state when the component mounts.
    - Integrated useSelector for accessing the address from the Redux store.
    - Updated navigation to redirect to the checkout page upon address selection.

--- src/components/AddressPage.tsx
+++ src/components/AddressPage.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { addressList } from '../data/address';
 import type IAddress from '../models/address';
 import { setAddress } from '../store/checkoutSlice';
+import type store from '../store/store';
 
 const AddressPage = () => {
   const [selectedId, setSelectedId] = useState<string | null>(null);
+  const storedAddress = useSelector((state: ReturnType<typeof store.getState>) => state.checkout.address);
   const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  // Select and display stored address by default
+  useEffect(() => {
+    if (storedAddress) {
+      setSelectedId(storedAddress.id);
+    }
+  }, [storedAddress]);
 
   const handleClick = () => {
     if (!selectedId) return;
@@ -15,6 +27,7 @@ const AddressPage = () => {
     if (!selectedAddress) return;
 
     dispatch(setAddress(selectedAddress));
+    navigate('/checkout');
   };
 
   return (
@@ -39,9 +52,9 @@ const AddressPage = () => {
 
       <button
         onClick={handleClick}
-        disabled={!selectedId}
         className={`mt-6 w-full py-3 text-white font-medium transition
           ${selectedId ? 'bg-green-600 hover:bg-green-700 shadow-sm cursor-pointer' : 'bg-gray-300 cursor-not-allowed'}`}
+        disabled={!selectedId}
       >
         Continue
       </button>
