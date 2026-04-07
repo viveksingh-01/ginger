1b3f9e99066f5e1943ff73cad5998a35ae68e3f7
Vivek Singh <viveksingh9301@gmail.com>
2026-04-08 00:07:00 +0530

    Implement login functionality with error handling and token storage upon successful authentication

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,11 +1,9 @@
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import Input from '../../../shared/components/Input';
-
-type FormData = {
-  phone: string;
-  password: string;
-};
+import { login } from '../api/auth';
+import type { ILoginPayload } from '../models/payload';
+import type IAuthResponse from '../models/response';
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -14,10 +12,16 @@ const LoginPage = () => {
     control,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm<FormData>();
+  } = useForm<ILoginPayload>();
 
-  const onSubmit = (data: FormData) => {
-    console.log('Login success', data);
+  const onSubmit = async (formData: ILoginPayload) => {
+    try {
+      const { data }: IAuthResponse = await login(formData);
+      localStorage.setItem('token', data.token);
+      navigate('/');
+    } catch (err: any) {
+      console.error(err.message);
+    }
   };
 
   return (
