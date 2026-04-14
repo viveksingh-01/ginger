01c5962f483561ca35797c2e8062510d15f1a231
Vivek Singh <viveksingh9301@gmail.com>
2026-04-14 16:02:00 +0530

    Add loading state management to authSlice
    - Introduced a loading state in the authSlice to track authentication status.
    - Added setLoading action to update the loading state based on authentication processes.
    - Updated setAuth and setUser actions to set loading to false upon successful updates.

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -4,11 +4,13 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 type AuthState = {
   user: IUser | null;
   token: string | null;
+  loading: boolean;
 };
 
 const initialState: AuthState = {
   user: null,
   token: localStorage.getItem('token'),
+  loading: true,
 };
 
 const authSlice = createSlice({
@@ -18,15 +20,21 @@ const authSlice = createSlice({
     setAuth: (state, action: PayloadAction<{ user: IUser; token: string }>) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
+      state.loading = false;
 
       localStorage.setItem('token', action.payload.token);
     },
 
     setUser: (state, action: PayloadAction<IUser>) => {
       state.user = action.payload;
+      state.loading = false;
+    },
+
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
     },
   },
 });
 
-export const { setAuth, setUser } = authSlice.actions;
+export const { setAuth, setUser, setLoading } = authSlice.actions;
 export default authSlice.reducer;
