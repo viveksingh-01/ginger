225130b482f83e91af9cc819f1ee36eff169cb94
Vivek Singh <viveksingh9301@gmail.com>
2026-04-14 16:11:34 +0530

    Integrate authentication initialization and loading state in App component
    - Added useEffect to dispatch initAuth on component mount for user authentication.
    - Implemented loading state management to display a loading indicator while authentication is in progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import { initAuth } from './features/auth/api/auth.init';
 import Navbar from './shared/components/Navbar';
+import type { AppDispatch, RootState } from './store/store';
 
 export default function App() {
+  const dispatch = useDispatch.withTypes<AppDispatch>()();
+  const loading = useSelector.withTypes<RootState>()(state => state.auth.loading);
+
+  useEffect(() => {
+    dispatch(initAuth());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div className="h-screen flex items-center justify-center">Loading...</div>;
+  }
+
   return (
     <>
       <Navbar />
