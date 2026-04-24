e9699274b816a456a173d423722c65597dcb0200
Vivek Singh <viveksingh9301@gmail.com>
2026-04-24 11:54:35 +0530

    Update cartSlice to use ICartItem type and add setItems action
    - Changed items type in CartState from IMenuItem[] to ICartItem[] for better type consistency.
    - Introduced setItems action to allow setting multiple cart items at once, facilitating initial load and rollback.
    - Updated setRestaurant action to store restaurantId in localStorage for persistence.

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,10 +1,9 @@
 import type ICartItem from '@/features/checkout/models/cart-item';
-import type { IMenuItem } from '@/features/menu/models/menu';
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 type CartState = {
   restaurantId: string;
-  items: IMenuItem[];
+  items: ICartItem[];
 };
 
 const initialState: CartState = {
@@ -19,17 +18,25 @@ const cartSlice = createSlice({
     addItem: (state: CartState, action: PayloadAction<ICartItem>) => {
       state.items.push(action.payload);
     },
+
     removeItem: (state: CartState, action: PayloadAction<ICartItem>) => {
       const index = state.items.findIndex(item => item.id === action.payload.id);
       if (index >= 0) {
         state.items.splice(index, 1);
       }
     },
+
+    // needed for initial load + rollback
+    setItems: (state, action: PayloadAction<ICartItem[]>) => {
+      state.items = action.payload;
+    },
+
     setRestaurant: (state: CartState, action: PayloadAction<string>) => {
       state.restaurantId = action.payload;
+      localStorage.setItem('restaurantId', action.payload);
     },
   },
 });
 
-export const { addItem, removeItem, setRestaurant } = cartSlice.actions;
+export const { addItem, removeItem, setItems, setRestaurant } = cartSlice.actions;
 export default cartSlice.reducer;
