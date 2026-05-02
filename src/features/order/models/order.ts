e775cb3b4018f11a1cee6aead8f19e198744dcc7
Vivek Singh <viveksingh9301@gmail.com>
2026-05-02 12:41:42 +0530

    Add IOrderDetail and IOrderItem interfaces for order management
    - Introduced IOrderDetail interface to define the structure of order details, including properties such as id, restaurantId, address, items, and billDetails.
    - Added IOrderItem interface to represent individual items within an order, enhancing type safety and clarity for order-related data handling in the application.

---
+++ src/features/order/models/order.ts
@@ -0,0 +1,23 @@
+import type IAddress from '@/features/address/models/address';
+import type { IBillDetails } from '@/features/checkout/models/cart-response';
+
+export default interface IOrderDetail {
+  id: string;
+  restaurantId: string;
+  restaurantName: string;
+  address: IAddress;
+  items: IOrderItem[];
+  billDetails: IBillDetails;
+  status: string;
+}
+
+export interface IOrderItem {
+  cloudinaryImageId: string;
+  finalPrice: number;
+  isVeg: number;
+  menuItemId: string;
+  name: string;
+  quantity: number;
+  strikeOffEnabled: boolean;
+  total: number;
+}
