92cf5f5c79949ce6dbdd25cbd0fce51b774f12e3
Vivek Singh <viveksingh9301@gmail.com>
2026-05-21 19:03:13 +0530

    Integrate react-query into main application structure
    - Added QueryClient and QueryClientProvider to main.tsx to enable data fetching and state management capabilities.
    - Wrapped RouterProvider with QueryClientProvider to ensure all components have access to the query client.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AddressPage from './features/address/pages/AddressPage.tsx';
 import SaveAddressPage from './features/address/pages/SaveAddressPage.tsx';
 import ProtectedRoutes from './features/auth/components/ProtectedRoutes.tsx';
@@ -50,10 +51,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
