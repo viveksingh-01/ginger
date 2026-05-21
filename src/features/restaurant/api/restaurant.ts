c71e97b647b0529758a18373090510a6d1342d44
Vivek Singh <viveksingh9301@gmail.com>
2026-05-21 19:05:42 +0530

    Add restaurant API fetch functionality
    - Created a new file restaurant.ts to implement the fetchRestaurants function.
    - The function retrieves restaurant data from the API and handles errors appropriately, returning an array of restaurants.

---
+++ src/features/restaurant/api/restaurant.ts
@@ -0,0 +1,19 @@
+import type IRestaurant from '../models/restaurant';
+
+const BASE_URL = import.meta.env.VITE_GINGER_API_URL;
+
+interface IRestaurantResponse {
+  data: IRestaurant[];
+}
+
+export const fetchRestaurants = async (): Promise<IRestaurant[]> => {
+  const res = await fetch(`${BASE_URL}/restaurants`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch restaurant data');
+  }
+
+  const result: IRestaurantResponse = await res.json();
+
+  return result.data || [];
+};
