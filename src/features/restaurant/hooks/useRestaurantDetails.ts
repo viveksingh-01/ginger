1dbf4af9eedfe53704149ed6bef9806480729542
Vivek Singh <viveksingh9301@gmail.com>
2026-05-23 23:14:35 +0530

    Refactor useRestaurantDetails hook to utilize react-query for fetching restaurant menu
    - Replaced manual fetching logic with useQuery from react-query to streamline data retrieval.
    - Removed local state management for restaurant data and loading state, enhancing performance and code clarity.

--- src/features/restaurant/hooks/useRestaurantDetails.ts
+++ src/features/restaurant/hooks/useRestaurantDetails.ts
@@ -1,25 +1,12 @@
-import { useEffect, useState } from 'react';
-import type IRestaurantWithMenu from '../models/restaurant-menu';
-
-const BASE_URL = import.meta.env.VITE_GINGER_API_URL;
+import { fetchRestaurantMenu } from '@/features/menu/api/restaurant-menu';
+import { useQuery } from '@tanstack/react-query';
 
 const useRestaurantDetails = (restaurantId: string) => {
-  const [restaurant, setRestaurant] = useState<IRestaurantWithMenu | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (!restaurantId) return;
-    fetchRestaurantMenu();
-  }, [restaurantId]);
-
-  const fetchRestaurantMenu = async () => {
-    const response = await fetch(`${BASE_URL}/restaurants/${restaurantId}/menu`);
-    const data: IRestaurantWithMenu = await response.json();
-    setRestaurant(data);
-    setIsLoading(false);
-  };
-
-  return { restaurant, isLoading };
+  return useQuery({
+    queryKey: ['restaurantMenu', restaurantId],
+    queryFn: () => fetchRestaurantMenu(restaurantId),
+    enabled: Boolean(restaurantId),
+  });
 };
 
 export default useRestaurantDetails;
