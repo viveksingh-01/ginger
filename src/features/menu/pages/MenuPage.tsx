9beb67bd74b5af13db30b2416d4341d106b43b50
Vivek Singh <viveksingh9301@gmail.com>
2026-05-23 23:19:01 +0530

    fix: Update MenuPage to handle optional chaining for restaurant details
    - Added optional chaining to restaurant details properties to prevent potential runtime errors when data is not available.

--- src/features/menu/pages/MenuPage.tsx
+++ src/features/menu/pages/MenuPage.tsx
@@ -20,19 +20,19 @@ const MenuPage: React.FC = () => {
     <section className="mt-16 bg-gray-50 dark:bg-gray-950 min-h-screen">
       <div className="container mx-auto px-6 py-10 max-w-3xl">
         <div className="mb-12">
-          <h2 className="my-3 text-3xl font-bold text-gray-900 dark:text-white">{restaurant?.details.name}</h2>
+          <h2 className="my-3 text-3xl font-bold text-gray-900 dark:text-white">{restaurant?.details?.name}</h2>
           <div className="p-3 bg-white/70 backdrop-blur rounded-lg">
             <div className="mb-1 font-semibold flex items-center gap-1">
               <FaStar className="text-green-700" />
-              {restaurant?.details.avgRatingString} ({restaurant?.details.totalRatingsString} ratings){' '}
+              {restaurant?.details?.avgRatingString} ({restaurant?.details?.totalRatingsString} ratings){' '}
               <FaCircle size={4} className="mx-1 text-gray-500" />
-              {restaurant?.details.costForTwo}
+              {restaurant?.details?.costForTwo}
             </div>
-            <div className="mb-1 text-ginger text-sm font-semibold">{restaurant?.details.cuisines.join(', ')}</div>
+            <div className="mb-1 text-ginger text-sm font-semibold">{restaurant?.details?.cuisines.join(', ')}</div>
             <div className="mb-1 text-sm text-gray-700 font-semibold flex items-center gap-1">
-              {restaurant?.details.locality}
+              {restaurant?.details?.locality}
               <FaCircle size={4} className="mx-2 text-gray-500" />
-              {restaurant?.details.sla.slaString}
+              {restaurant?.details?.sla.slaString}
             </div>
           </div>
         </div>
