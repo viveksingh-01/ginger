8baafd580f135fa86b0b91a017e1087d6efeafad
Vivek Singh <viveksingh9301@gmail.com>
2026-06-26 21:07:46 +0530

    feat: Calculate total amount in useCartDetails hook
    - Added logic to compute the total amount of cart items by summing their final prices, enhancing the functionality of the useCartDetails hook. This allows for better tracking of the cart's total value.

--- src/features/payment/hooks/useCartDetails.ts
+++ src/features/payment/hooks/useCartDetails.ts
@@ -3,8 +3,14 @@ import { useSelector } from 'react-redux';
 
 export const useCartDetails = () => {
   const cartItems = useSelector((state: ReturnType<typeof store.getState>) => state.cart.items);
+  let totalAmount = 0;
+
+  for (const { finalPrice } of cartItems) {
+    totalAmount += finalPrice / 100;
+  }
 
   return {
     count: cartItems.length,
+    totalAmount,
   };
 };
