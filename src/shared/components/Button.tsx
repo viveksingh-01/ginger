0ab63d48c1d96bc68788a2121f995e3ccc3c4226
Vivek Singh <viveksingh9301@gmail.com>
2026-07-01 22:02:50 +0530

    feat: Add Button component
    - Introduced a new Button component to provide a consistent call-to-action element throughout the application, enhancing user interaction with a styled button.

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button className="w-full py-4 text-sm font-semibold text-white bg-ginger hover:bg-ginger-dark cursor-pointer">
+      CTA
+    </button>
+  );
+};
+
+export default Button;
