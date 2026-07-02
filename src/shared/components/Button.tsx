2f053d5871567694e19acb0fc365d5cd5ef7a7b5
Vivek Singh <viveksingh9301@gmail.com>
2026-07-02 22:50:58 +0530

    feat: Add isDisabled prop to Button component
    - Enhanced the Button component by introducing an isDisabled prop, allowing buttons to be conditionally disabled

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,13 +1,15 @@
 type ButtonProps = {
   children: string;
+  isDisabled: boolean;
   onClickHandler: () => void;
 };
 
-const Button: React.FC<ButtonProps> = ({ children, onClickHandler }) => {
+const Button: React.FC<ButtonProps> = ({ children, isDisabled, onClickHandler }) => {
   return (
     <button
       onClick={onClickHandler}
       className="w-full py-4 text-sm font-semibold text-white bg-ginger hover:bg-ginger-dark cursor-pointer transition"
+      disabled={isDisabled}
     >
       {children}
     </button>
